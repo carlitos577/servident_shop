85aaafeb84be46af609f6dbae05faf63462e6afe
carlitos577 <109829586+carlitos577@users.noreply.github.com>
2024-04-25 14:55:45 -0400

    Update settings.py

--- AppWebServiDent/settings.py
+++ AppWebServiDent/settings.py
@@ -32,7 +32,7 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
