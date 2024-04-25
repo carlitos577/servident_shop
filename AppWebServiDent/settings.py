e9ffcfc87886e0476172392046c307a0955c59ae
carlitos577 <109829586+carlitos577@users.noreply.github.com>
2024-04-25 13:08:54 -0400

    Update settings.py

--- AppWebServiDent/settings.py
+++ AppWebServiDent/settings.py
@@ -92,13 +92,6 @@ WSGI_APPLICATION = 'AppWebServiDent.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }    
-}
-
-DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost/postgres',
